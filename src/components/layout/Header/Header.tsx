250418ebee2dbf89b1e83dae7ff7a0f75adfc5ff
dtalledo18 <dtalledo@advancedteamelite.com>
2026-03-09 12:20:19 -0500

    fix: header position

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,13 +4,17 @@ export default function Header() {
     return (
         <header className={styles.header}>
             <img src="/logo.svg" alt="309T Logo" className={styles.logo} />
-            <nav className={styles.nav}>
-                <a href="#about">About us</a>
-                <a href="#features">Features</a>
-                <a href="#values">Values</a>
-                <a href="#faq">FAQ</a>
-            </nav>
-            <button className={styles.btnWaitlist}>Join the Waitlist</button>
+
+            {/* Este contenedor agrupa nav y botón para enviarlos juntos a la derecha */}
+            <div style={{ display: 'flex', alignItems: 'center', gap: '40px' }}>
+                <nav className={styles.nav}>
+                    <a href="#about">About us</a>
+                    <a href="#features">Features</a>
+                    <a href="#values">Values</a>
+                    <a href="#faq">FAQ</a>
+                </nav>
+                <button className={styles.btnWaitlist}>Join the Waitlist</button>
+            </div>
         </header>
     );
 }
